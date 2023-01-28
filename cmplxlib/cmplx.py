306a1d56d3e79f51d9d8d63a39a0795336ee790f
VizcaGod <102333646+VizcaGod@users.noreply.github.com>
2023-01-28 15:23:42 -0500

    Add files via upload

--- cmplxlib/cmplx.py
+++ cmplxlib/cmplx.py
@@ -1,7 +1,7 @@
 import math
 
 def Sumcplx(c1, c2):
-    """Funcion que realiza la suma de dos numeros complejos y retorna
+    """ Funcion que realiza la suma de dos numeros complejos y retorna
     un numero complejo
     (tuple, tuple) -> tuple"""
     real = c1[0] + c2[0]
@@ -15,7 +15,7 @@ def PrettyPcplx(c):
     print("{} + {}i".format(c[0], c[1]))
 
 def Multcplx(c1,c2):
-    """Funcion que realiza la multiplicacion de dos numeros complejos
+    """ Funcion que realiza la multiplicacion de dos numeros complejos
     y retorna un numero complejo
     (tuple, tuple) -> tuple"""
     real = c1[0] * c2[0] - c1[1] * c2[1]
@@ -24,7 +24,7 @@ def Multcplx(c1,c2):
     return c
 
 def Divcplx(c1,c2):
-    """Funcion que realiza la division de dos numeros complejos
+    """ Funcion que realiza la division de dos numeros complejos
     y retorna un numero complejo
     (tuple, tuple) -> tuple"""
     real = ((c1[0]*c2[0])+(c1[1]*c2[1]))/((c2[0]**2)+(c2[1]**2))
@@ -33,21 +33,36 @@ def Divcplx(c1,c2):
     return c
 
 def Modulocplx(c):
-    """Funcion que calcula el modulo de un numero complejo
+    """ Funcion que calcula el modulo de un numero complejo
     (tuple) -> float"""
     modul = float(math.sqrt(c[0]**2+c[1]**2)
     return modul
 
-def conjugadocplx(c):
-    """Funcion que calcula el conjugado de un numero complejo
+def Conjugadocplx(c):
+    """ Funcion que calcula el conjugado de un numero complejo
     (tuple) -> float"""
     n = (c[0], c[1]*-1)
     return n
 
-def polarcplx(c):
-    norma = round(math.sqrt(c[0]**2+c[1]**2), 3)
-    angulo = round(math.atan2(c[1], c[0]), 3)
+def Polarcplx(c):
+    """ Funcion que obtiene las coordenadas polares de un numero complejo
+    tuple -> tuple"""
+    norma = math.sqrt(c[0]**2+c[1]**2)
+    angulo = math.atan2(c[1], c[0])
     return norma, angulo
+
+def Cartesianascplx(c):
+    """ Funcion que obtiene las coordenadas polares de un numero complejo
+    tuple -> tuple"""
+    a = c[0] * math.cos(c[1])
+    b = c[0] * math.sin(c[1])
+    return a, b
+
+def Fasecplx(c):
+    """funcion que obtiene la fase de numeros complejos
+    tuple -> tuple"""
+    angulo = math.atan2(c[1], c[0])
+    return angulo
 
 if __name__ == "__main__":
     #test enviar cmplx, test, readme, gitignore
